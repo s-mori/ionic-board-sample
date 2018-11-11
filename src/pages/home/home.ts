fff69c3281c4b811f115fd3d1ffe34177b52aef9
s-mori <b1010187+ghub@gmail.com>
2018-11-11 20:52:31 +0900

    fix form, add addPost

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -16,6 +16,7 @@ export class HomePage {
     message: 'これはテストメッセージです',
     createdDate: '10分前'
   };
+  message: string;
 
   posts: { userName: string, message: string, createdDate: any }[]
     = [
@@ -33,6 +34,19 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  addPost() {
+    // 入力されたメッセージを使って、投稿データを作成
+    this.post = {
+      userName: 'Saburo Hogeyama',
+      message: this.message,
+      createdDate: '数秒前'
+    };
+    // 配列postsにpostを追加
+    this.posts.push(this.post);
+    // 入力フィールドを空にする
+    this.message = '';
   }
 
 }
