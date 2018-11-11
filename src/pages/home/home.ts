c94f666d28c34e27dd3709c7cc0884b18c7e3fea
s-mori <b1010187+ghub@gmail.com>
2018-11-11 17:13:57 +0900

    display messages

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  post: {
+    userName: string,
+    message: string,
+    createdDate: any
+  } = {
+    userName: 'Taro Hogeyama',
+    message: 'これはテストメッセージです',
+    createdDate: '10分前'
+  };
+
+  posts: { userName: string, message: string, createdDate: any }[]
+    = [
+      {
+        userName: 'Taro Hogeyama',
+        message: 'これはテストメッセージです',
+        createdDate: '10分前'
+      },
+      {
+        userName: 'Jiro Hogeyama',
+        message: '2つ目のメッセージ',
+        createdDate: '5分前'
+      }
+    ];
+
   constructor(public navCtrl: NavController) {
 
   }
